66bd71edc5c54e1e7cf1caf409d3040e0d137b50
NazimAbdellaoui <abdellaouinazimn@gmail.com>
2024-05-30 11:33:40 +0200

    test all LED

--- examples-api-use/minimal-example.cc
+++ examples-api-use/minimal-example.cc
@@ -8,38 +8,17 @@
 #include "led-matrix.h"
 
 #include <unistd.h>
-#include <math.h>
 #include <stdio.h>
 #include <signal.h>
 
 using rgb_matrix::RGBMatrix;
 using rgb_matrix::Canvas;
 
-volatile bool interrupt_received = false;
-static void InterruptHandler(int signo) {
-  interrupt_received = true;
-}
-
 static void DrawOnCanvas(Canvas *canvas) {
   /*
-   * Let's create a simple animation. We use the canvas to draw
-   * pixels. We wait between each step to have a slower animation.
+   * We simply fill the entire canvas with a color.
    */
-  canvas->Fill(0, 0, 255);
-
-  int center_x = canvas->width() / 2;
-  int center_y = canvas->height() / 2;
-  float radius_max = canvas->width() / 2;
-  float angle_step = 1.0 / 360;
-  for (float a = 0, r = 0; r < radius_max; a += angle_step, r += angle_step) {
-    if (interrupt_received)
-      return;
-    float dot_x = cos(a * 2 * M_PI) * r;
-    float dot_y = sin(a * 2 * M_PI) * r;
-    canvas->SetPixel(center_x + dot_x, center_y + dot_y,
-                     255, 0, 0);
-    usleep(1 * 1000);  // wait a little to slow down things.
-  }
+  canvas->Fill(255, 0, 0);  // Fill the entire matrix with red color (255, 0, 0)
 }
 
 int main(int argc, char *argv[]) {
@@ -53,15 +32,14 @@ int main(int argc, char *argv[]) {
   if (canvas == NULL)
     return 1;
 
-  // It is always good to set up a signal handler to cleanly exit when we
-  // receive a CTRL-C for instance. The DrawOnCanvas() routine is looking
-  // for that.
-  signal(SIGTERM, InterruptHandler);
-  signal(SIGINT, InterruptHandler);
+  DrawOnCanvas(canvas);  // Using the canvas.
 
-  DrawOnCanvas(canvas);    // Using the canvas.
+  // Wait indefinitely
+  while (true) {
+    usleep(1000 * 1000);  // Sleep for 1 second intervals to keep the program running.
+  }
 
-  // Animation finished. Shut down the RGB matrix.
+  // Cleanup (This part will never be reached in this version, but kept for completeness)
   canvas->Clear();
   delete canvas;
 
